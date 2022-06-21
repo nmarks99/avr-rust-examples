827b0aaf5856027d3566cf107c13582e6c6c7f32
Nick <nmarks99@gmail.com>
2022-06-20 23:57:37 -0500

    save

--- proto/nano-proto/src/main.rs
+++ proto/nano-proto/src/main.rs
@@ -28,24 +28,22 @@ pub unsafe fn set_bit(addr: *mut u8, bit: u8, state: bool) {
 
 }
 
-// pub unsafe fn usart_init() {
-//     set_bit(UCSR0A,0x2,false); // sets bit 1, the second bit (U2X0=0x2), to zero 
+pub unsafe fn usart_init() {
+    set_bit(UCSR0A,0x2,false); // sets bit 1, the second bit (U2X0=0x2), to zero 
     
-//     // Set to 8 bit data size
-//     set_bit(UCSR0C,0x2,true); // UCSZ00 = 1
-//     set_bit(UCSR0C,0x3,true); // UCSZ01 = 1
-//     set_bit(UCSR0B,0x3,false); //UCSZ02 = 0
+    // Set to 8 bit data size
+    set_bit(UCSR0C,0x2,true); // UCSZ00 = 1
+    set_bit(UCSR0C,0x3,true); // UCSZ01 = 1
+    set_bit(UCSR0B,0x3,false); //UCSZ02 = 0
 
-//     // Enable receiver and transitter
-//     set_bit(UCSR0B,0x4,true); // Tx enable
-//     set_bit(UCSR0B,0x5,true); // Rx enable
+    // Enable receiver and transitter
+    set_bit(UCSR0B,0x4,true); // Tx enable
+    set_bit(UCSR0B,0x5,true); // Rx enable
 
-//     // Set baudrate
-//     // let BAUD = ((8000000 / 115200) / 16) - 1;
-//     // set_bit(UBRR0H,)
-
-
-// }
+    // Set baudrate
+    // let BAUD = ((8000000 / 115200) / 16) - 1;
+    // set_bit(UBRR0H,)
+}
 
 
 
