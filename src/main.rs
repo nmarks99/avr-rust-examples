cffa2387b105c7f30d73e513cf39b46bc8084a42
Nick <nmarks99@gmail.com>
2022-06-21 09:27:07 -0500

    Save

--- src/main.rs
+++ src/main.rs
@@ -4,51 +4,41 @@
 #![allow(dead_code)]
 #![allow(non_snake_case)]
 
-use core::{panic::PanicInfo};
 mod atmega328p; use atmega328p::*;
 use core::intrinsics::volatile_store;
 
-
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+pub fn LS1(bit: u8) -> u8 {
+    // Left shifts 1 the specfied amount
+    1 << (bit)
 }
 
+pub unsafe fn set_bit(addr: *mut u8, bit: u8, state: bool) {
+    let val: u8;
+    match state {
+        true => val = *addr | LS1(bit),
+        false => val = *addr & !LS1(bit)
+    }
+    volatile_store(addr,val);
 
+}
 
-//
-// pub fn LS1(bit: u8) -> u8 {
-    // // Left shifts 1 the specfied amount
-    // 1 << (bit)
-// }
-//
-// pub unsafe fn set_bit(addr: *mut u8, bit: u8, state: bool) {
-    // let val: u8;
-    // match state {
-        // true => val = *addr | LS1(bit),
-        // false => val = *addr & !LS1(bit)
-    // }
-    // volatile_store(addr,val);
-//
-// }
-//
-// pub unsafe fn usart_init() {
-    // set_bit(UCSR0A,0x2,false); // sets bit 1, the second bit (U2X0=0x2), to zero
-    //
-    // // Set to 8 bit data size
-    // set_bit(UCSR0C,0x2,true); // UCSZ00 = 1
-    // set_bit(UCSR0C,0x3,true); // UCSZ01 = 1
-    // set_bit(UCSR0B,0x3,false); //UCSZ02 = 0
-//
-    // // Enable receiver and transitter
-    // set_bit(UCSR0B,0x4,true); // Tx enable
-    // set_bit(UCSR0B,0x5,true); // Rx enable
-//
-    // // Set baudrate
-    // // let BAUD = ((8000000 / 115200) / 16) - 1;
-    // // set_bit(UBRR0H,)
-// }
-//
+pub unsafe fn usart_init() {
+    set_bit(UCSR0A,0x2,false); // sets bit 1, the second bit (U2X0=0x2), to zero
+
+    // Set to 8 bit data size
+    set_bit(UCSR0C,0x2,true); // UCSZ00 = 1
+    set_bit(UCSR0C,0x3,true); // UCSZ01 = 1
+    set_bit(UCSR0B,0x3,false); //UCSZ02 = 0
+
+    // Enable receiver and transitter
+    set_bit(UCSR0B,0x4,true); // Tx enable
+    set_bit(UCSR0B,0x5,true); // Rx enable
+
+    // Set baudrate
+    // let BAUD = ((8000000 / 115200) / 16) - 1;
+    // set_bit(UBRR0H,)
+}
+
 
 
 #[arduino_hal::entry]
