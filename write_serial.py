804700f6a4a2adfbea0b99ed3ee62c6039cabf4f
Nick <nmarks99@gmail.com>
2022-06-23 13:56:46 -0500

    update

--- write_serial.py
+++ write_serial.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 import serial
 ser = serial.Serial('/dev/ttyUSB0')
-print(ser.name)
-ser.write(b'hello\0')
-ser.close()
+
+
+try:
+    while True:
+        msg = input(">> ");
+        ser.write(b'{}\n'.format(msg))
+except KeyboardInterrupt: 
+    ser.close()
+    
+
+
+
