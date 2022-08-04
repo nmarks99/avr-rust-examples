6d61644c9cab2167489a46fa9e865975cb76eb63
Nick <nmarks99@gmail.com>
2022-08-04 16:33:18 -0500

    improvement to usart

--- how_about_c/raw-nano/src/usart.h
+++ how_about_c/raw-nano/src/usart.h
@@ -1,4 +1,7 @@
 #ifndef __USART__H__
+#define __USART__H__
+
+#include<string.h>
 
 #ifndef F_CPU
 #define F_CPU 16000000UL
@@ -8,6 +11,7 @@
 
 #ifndef BAUD
 #define BAUD 9600
+
 #endif
 
 #include <util/setbaud.h>
@@ -16,7 +20,9 @@
 // functions
 void usart_init(void);
 void usart_send_byte(char val);
-void usart_send_str(char *str, uint16_t len);
+void usart_print(char *str);
+void usart_println(char *str);
+void usart_newline();
 char read_byte(void);
 void read_str(char *buff);
 
