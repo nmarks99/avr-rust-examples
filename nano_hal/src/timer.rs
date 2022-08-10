4c1c248b9c2f9e665d3f385a288960066096e063
Nick <nmarks99@gmail.com>
2022-08-09 20:05:17 -0500

    Save

--- nano_hal/src/timer.rs
+++ nano_hal/src/timer.rs
@@ -1,8 +1,10 @@
 use crate::atmega328p::*;
 use core::ptr::read_volatile;
 use core::ptr::write_volatile;
-use micromath::F32Ext;
 
+pub const T1: Timer = Timer{pre: 64};
+pub const MAX_TICKS: u32 = 65536;
+pub const TICKS_PER_MS: u8 = 250;
 
 // For now this is just timer1 which is a 16 bit timer
 pub struct Timer {
@@ -41,97 +43,5 @@ impl Timer {
             write_volatile(TIFR1, 1 << *TOV1);
             true 
         }
-        
     }
-
 }
-
-pub const T1: Timer = Timer{pre: 64};
-pub const MAX_TICKS: u32 = 65536;
-pub const TICKS_PER_MS: u8 = 250;
-
-// #[macro_export]
-// macro_rules! delay {
-//     ($ms:expr) => {
-//         let desired_ticks: u32 = (ms*TICKS_PER_MS) as u32;
-//         let desired_overflows: u8 = (((desired_ticks/MAX_TICKS) as f32).floor() ) as u8;
-//         let remaining_ticks: u32 =  desired_ticks % MAX_TICKS;
-//         let mut current_overflow: u8 = 0; 
-//         let mut current_ticks: u32;
-        
-//         T1.init();
-        
-//         loop {
-
-//             current_ticks = T1.get_count() as u32;
-            
-//             if desired_overflows > 0  {
-//                 if T1.overflow_flag() == true {
-//                     if current_overflow < desired_overflows{
-//                         current_overflow += 1;
-//                         T1.reset();
-//                     }
-//                     else {
-//                     if current_ticks >= remaining_ticks {
-//                             break
-//                     }
-//                     }
-//                 } 
-//             }
-
-//             else {
-//                 if current_ticks >= desired_ticks {
-//                     break
-//                 }
-//             }
-            
-//         }
-
-        
-//     };
-//     () => {
-//         loop {};
-//     };
-// }
-
-
-// pub unsafe fn delay(ms: f32) {
-    
-//     // let desired_ticks = 62500;
-//     // let desired_overflows = 0;
-//     // let remaining_ticks = 0;
-
-//     let desired_ticks: u32 = (ms*TICKS_PER_MS) as u32;
-//     let desired_overflows: u8 = (((desired_ticks/MAX_TICKS) as f32).floor() ) as u8;
-//     let remaining_ticks: u32 =  desired_ticks % MAX_TICKS;
-//     let mut current_overflow: u8 = 0; 
-//     let mut current_ticks: u32;
-
-//     T1.init();
-    
-//     loop {
-
-//         current_ticks = T1.get_count() as u32;
-        
-//         if desired_overflows > 0  {
-//             if T1.overflow_flag() == true {
-//                 if current_overflow < desired_overflows{
-//                     current_overflow += 1;
-//                     T1.reset();
-//                 }
-//                 else {
-//                 if current_ticks >= remaining_ticks {
-//                         break
-//                 }
-//                 }
-//             } 
-//         }
-
-//         else {
-//             if current_ticks >= desired_ticks {
-//                 break
-//             }
-//         }
-        
-//     }
-// }
