169d87bc0898075ae5ad5976297413ac07180123
Nick <nmarks99@gmail.com>
2022-08-10 12:11:57 -0500

    Save

--- C_version/src/main.c
+++ C_version/src/main.c
@@ -16,6 +16,15 @@
     /* sei(); */
 /* } */
 
+void set_output(void) {
+    DDRB |= (1 << DDB5); // set pin B5 as ouptut
+}
+void set_low(void) {
+    PORTB &= ~(1 << PORTB5); // set low
+}
+void set_high(void) {
+    PORTB |= (1 << PORTB5); // set high
+}
 
 int main(void) {   
    
@@ -23,10 +32,8 @@ int main(void) {
     /* timer1_init(); */
     /* usart_init(); */
     /* char m[30]; */
-    DDRB |= (1 << DDB5); // set pin B5 as ouptut
-    /* PORTB |= (1 << PORTB5); // set high */
-    PORTB &= ~(1 << PORTB5); // set low
-    
+    set_output();
+    set_high();
     while (1) {  
         /* unsigned long t0 = millis(); */
         /* _delay_ms(320); */
