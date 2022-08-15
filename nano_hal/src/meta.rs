aa98031dbde240be24adb9665d9353c70b152733
Nick <nmarks99@gmail.com>
2022-08-15 09:20:53 -0500

    Save

--- nano_hal/src/meta.rs
+++ nano_hal/src/meta.rs
@@ -1,12 +1,6 @@
 use core::ptr::write_volatile;
-use core::llvm_asm;
 
-pub const F_CPU: u32 = 16000000; // 16 MHz
-
-
-
-
-
+pub const F_CPU: u32 = 16000000; // CPU clock freq = 16 MHz
 
 pub fn _BV(bit: u8) -> u8 {
     // Left shifts 1 the specfied amount
@@ -14,24 +8,10 @@ pub fn _BV(bit: u8) -> u8 {
 }
 
 pub unsafe fn set_bit(addr: *mut u8, bit: u8, state: bool) {
-    // sets 'bit' at address 'addr' to 1 ('state' = true) or 0 ('state' = false)
+    // sets bit 'bit' at address 'addr' to 1 ('state' = true) or 0 ('state' = false)
     let val: u8 = match state {
         true => *addr | _BV(bit),
         false => *addr & !_BV(bit)
     };
     write_volatile(addr,val);
-}
-
-
-
-
-
-
-// Interrupts
-pub unsafe fn cli() {
-    llvm_asm!("cli")
-}
-
-pub unsafe fn sei() {
-    llvm_asm!("sei");
 }
