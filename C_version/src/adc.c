84698abae6b4ce63702440b7662a8b80f8c85826
Nick <nmarks99@gmail.com>
2022-08-16 15:36:20 -0500

    Added untested ADC code

---
+++ C_version/src/adc.c
@@ -0,0 +1,29 @@
+#include "adc.h"
+
+void adc_init(void) {
+    // Connect capacitor between AREF and GND to reduce noise
+
+    ADMUX |= (1 << REFS0); // use AVcc as reference voltage
+
+    // Set prescaler to 128, enable ADC
+    // 16MHz/128 = 125kHz 
+    ADCSRA |= (1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2) | (1 << ADEN);
+}
+
+uint16_t adc_read(uint8_t chan){ 
+
+    // Select ADC channel, masking for safety
+    ADMUX = (ADMUX & 0xF0) | (chan & 0x0F); 
+    
+    // Set single conversion mode 
+    // setting ADSC to start the conversion
+    ADCSRA |= (1<<ADSC); 
+
+    // Wait for conversion to finish
+    // Will switch bag to 0 when its done
+    while( ADCSRA & (1 << ADSC) );
+
+    // Return value from ADC 
+    return ADC;
+
+}
---
+++ C_version/src/adc.h
@@ -0,0 +1,4 @@
+#include "meta.h"
+
+void adc_init(void);
+uint16_t adc_read(uint8_t chan);
