c8881a4ad1cf4124872f1881f42539edcdb5b648
Nick <nmarks99@gmail.com>
2022-08-24 14:29:39 -0500

    now just 1 directory

--- baremetal/C_version/src/meta.h
+++
@@ -1,9 +0,0 @@
-#include <avr/io.h>
-#include "usart.h"
-
-void panic(void);
-void panic_msg(char *msg);
-void led_set_output(void);
-void led_set_low(void);
-void led_set_high(void);
-void led_toggle(void);
--- examples/avr_hal_new.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-cargo generate --git https://github.com/Rahix/avr-hal-template.git
